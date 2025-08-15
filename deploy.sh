99fd0b97bf783388109b87b89b0e9283abcc8db4
Anya-Ayob <anyaayob2023@gmail.com>
2025-08-15 15:11:52 +0000

    Still issues: But modified deploy.sh, and the deployments for backend- and database. The service for backend has also changed. PersistentVolume was also added

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@ echo "${KUBECONFIG}" | base64 -d > kubeconfig
 
 export KUBECONFIG=kubeconfig
 
+#deploying persistent volume
+kubectl apply -f persistentVolume/pv-volume.yaml
+
 #deploying every deployment and services in the respective dirctories
 kubectl apply -f deployments/database-deployment.yaml
 kubectl apply -f deployments/backend-deployment.yaml
