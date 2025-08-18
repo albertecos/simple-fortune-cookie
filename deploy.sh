a653ccb5a102f0a787649afd7df397361515b90e
asta-strachotta <144019710+asta-strachotta@users.noreply.github.com>
2025-08-18 10:50:35 +0200

    Update deploy.sh to exclude the database-deployment

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ echo "${KUBECONFIG}" | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
 
 #deploying every deployment and services in the respective dirctories
-kubectl apply -f deployments/database-deployment.yaml
+# kubectl apply -f deployments/database-deployment.yaml
 kubectl apply -f deployments/backend-deployment.yaml
 kubectl apply -f deployments/frontend-deployment.yaml
 
