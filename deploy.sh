b948fccaa6e4855a5626b73139bce6883848fd04
asta-strachotta <144019710+asta-strachotta@users.noreply.github.com>
2025-08-18 10:59:00 +0200

    Update deploy.sh added back kubeconfig

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ export KUBECONFIG=kubeconfig
 
 #deploying every deployment and services in the respective dirctories
 # kubectl apply -f deployments/database-deployment.yaml
-kubectl apply -f deployments/backend-deployment.yaml
-kubectl apply -f deployments/frontend-deployment.yaml
+kubectl apply --kubeconfig kubeconfig -f deployments/backend-deployment.yaml
+kubectl apply --kubeconfig kubeconfig -f deployments/frontend-deployment.yaml
 
 kubectl apply -f services/
 
