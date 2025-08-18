b3bccfb62458d3599c00fdb7c7033b129457831a
albertecos <albertecosmus@gmail.com>
2025-08-18 06:28:02 +0000

    volumes deleted

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,6 @@ echo "${KUBECONFIG}" | base64 -d > kubeconfig
 
 export KUBECONFIG=kubeconfig
 
-#deploying persistent volume
-kubectl apply -f persistentVolume/pv-volume.yaml
-
 #deploying every deployment and services in the respective dirctories
 kubectl apply -f deployments/database-deployment.yaml
 kubectl apply -f deployments/backend-deployment.yaml
