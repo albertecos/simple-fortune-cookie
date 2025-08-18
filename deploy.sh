24abf7d134731bb587d1808268ad2f8cd96ac826
asta-strachotta <144019710+asta-strachotta@users.noreply.github.com>
2025-08-18 10:47:07 +0200

    Update deploy.sh to deploy in correct order

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
 echo "${KUBECONFIG}" | base64 -d > kubeconfig
 
 export KUBECONFIG=kubeconfig
 
 #deploying every deployment and services in the respective dirctories
-kubectl apply -f deployments/
+kubectl apply -f deployments/database-deployment.yaml
+kubectl apply -f deployments/backend-deployment.yaml
+kubectl apply -f deployments/frontend-deployment.yaml
+
 kubectl apply -f services/
 
 rm kubeconfig
